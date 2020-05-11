5bb5b36bd28d4b5ad4f55d0ad4cf87ed80d0a039
westhian <51913911+westhian@users.noreply.github.com>
2020-05-11 22:42:44 +0200

    Create KraftApp.tsx

---
+++ public/components/common/KraftApp.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const KraftAppNavigation: React.StatelessComponent<{}> = () => {
+  return (
+    <div>
+      <a href="https://getkraft.app/" target="_blank">
+        Übersicht
+      </a>
+      &middot;
+      <a href="https://getkraft.app/faq" target="_blank">
+        Häufige Fragen
+      </a>
+      &middot;
+      <a href="https://getkraft.app/privacy" target="_blank">
+        Datenschutzbestimmungen
+      </a>
+    </div>
+  );
+};
